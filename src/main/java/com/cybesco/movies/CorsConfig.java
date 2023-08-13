2fbb3dda0beca77fe12b9b47963e2d12b2c5b808
Emmanuel Ememe <ememeemmanuel3@gmail.com>
2023-08-13 09:03:59 +0100

    configure cors

---
+++ src/main/java/com/cybesco/movies/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.cybesco.movies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Adjust the mapping as needed
+            .allowedOrigins("http://localhost:3001") // Allow requests from this origin
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+            .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
+    }
+}
